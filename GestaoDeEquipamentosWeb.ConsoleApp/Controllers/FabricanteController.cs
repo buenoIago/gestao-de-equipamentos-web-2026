9120ff232304f6ac4ace3bd3f091033a271a1b9b
buenoIago <iagocedup@gmail.com>
2026-05-13 15:36:43 -0300

    Adicionado cadastro de Fabricante

--- GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -17,12 +17,32 @@ public class FabricanteController : Controller
          repositorioFabricante = 
             new RepositorioFabricanteEmArquivo(contexto);
     }
+ 
     // GET: FabricanteController
+    [HttpGet]
     public ActionResult Listar()
     {
         List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
 
         return View(fabricantes);
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(string nome, string email, string telefone)
+    {
+        Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+
+        repositorioFabricante.Cadastrar(novoFabricante);
+    
+        string listarStr = nameof(Listar); // "Listar"
+
+        return RedirectToAction("Listar");
     }
 
 }
