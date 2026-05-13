2660f3af9e6ddd90589cf7f0b91fd9ca8d8be39b
buenoIago <iagocedup@gmail.com>
2026-05-13 15:07:12 -0300

    Adicionada listagem de Fabricantes

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -0,0 +1,28 @@
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado.Arquivos;
+using GestaoDeEquipamentosWeb.ConsoleApp.ModuloFabricante;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers;
+
+// MVC -  Model , view 
+public class FabricanteController : Controller
+{
+    private readonly IRepositorio<Fabricante> repositorioFabricante;
+    public FabricanteController()
+    {
+        ContextoJson contexto = new ContextoJson();
+        contexto.Carregar();
+
+         repositorioFabricante = 
+            new RepositorioFabricanteEmArquivo(contexto);
+    }
+    // GET: FabricanteController
+    public ActionResult Listar()
+    {
+        List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
+
+        return View(fabricantes);
+    }
+
+}
