6d535bb9f3a7895eb9e7ceea7f405199cd7732f9
buenoIago <iagocedup@gmail.com>
2026-05-13 14:05:25 -0300

    Adicionado criação de  Servidor Web

--- GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// APS .NET core
+// Montar um  servidor web
+
+// Builder de um servidor web
+WebApplicationBuilder builder =  WebApplication.CreateBuilder(args);
+
+// MVC tipo de aplicação web, como vamos apresentar as informações para o usuário
+builder.Services.AddControllersWithViews();
+
+// Criação da instância do servidor web
+WebApplication app = builder.Build();
+
+// Middlewares - funções que executam em cada chamada que o nosso servidor vai receber
+app.UseRouting();
+app.MapDefaultControllerRoute();
+
+// Inicia o loop da aplicação
+app.Run();
+
