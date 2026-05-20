5bc748f0f847995bd0288677afcd763bb4242c74
buenoIago <iagocedup@gmail.com>
2026-05-20 14:29:23 -0300

    Adicionado html e csss relacionado a filtro de chamado

--- GestaoDeEquipamentosWeb.ConsoleApp/Controllers/ChamadoController.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/ChamadoController.cs
@@ -21,8 +21,10 @@ public class ChamadoController : Controller
     }
 
     [HttpGet]
-    public ActionResult Listar()
+    public ActionResult Listar(string? status)
     {
+        string? statusSelecionado = status?.ToLower();
+
         List<Chamado> chamados = repositorioChamado.SelecionarTodos();
 
         List<ListarChamadoViewModel> visualizarChamados = new List<ListarChamadoViewModel>();
@@ -41,6 +43,8 @@ public class ChamadoController : Controller
             visualizarChamados.Add(listarChamadoVm);
 
         }
+
+        ViewBag.StatusSelecionado = statusSelecionado;
 
         return View(visualizarChamados);
     }
